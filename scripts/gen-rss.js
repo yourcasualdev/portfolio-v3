f3b1fa109d37dd58c1459f75e7efe6a629456f7e
İbrahim BABAL <ibrahim.babal@anycivilization.com>
2022-12-27 23:04:53 +0300

    add: rss and footer values

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Your Casual Developer - İbrahim BABAL',
+    site_url: 'https://yourcasual.dev',
+    feed_url: 'https://yourcasual.dev/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
