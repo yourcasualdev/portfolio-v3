5e190283418cf1ce5fd5820584861e92dd301884
İbrahim BABAL <ibrahim.babal@anycivilization.com>
2022-12-27 23:16:16 +0300

    update metadata

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Your Casual Developer - İbrahim BABAL',
+    description: 'My personal website. I am a software developer. I write about web development, JavaScript, React, Next.js, Node.js, and more.',
+    image: 'https://avatars.githubusercontent.com/u/62076627?s=400&u=19923ba2c566c8a65bfa2bd39621bb523a39af48&v=4'
   }
 
   return (
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@nightvision53" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
